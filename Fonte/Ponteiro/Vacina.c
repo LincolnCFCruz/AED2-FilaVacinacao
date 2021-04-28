3bdb462bfa01f5b05bc9a70826ba9847652f8ef4
Pedro Igor <55034603+ORZAYAZ@users.noreply.github.com>
2021-04-28 16:24:11 -0300

    Add files via upload

---
+++ Fonte/Ponteiro/Vacina.c
@@ -0,0 +1,221 @@
+#include "Vacina.h"
+
+void ImprimirGrupos()
+{
+	printf("\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  | Grupos |                Informações                   |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    0   | Nenhum dos grupos                            |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    1   | Sistema Único de Assistência Social (SUAS),  |\n");
+	printf("  |        | das entidades e organizações de assistência  |\n");
+	printf("  |        | social, conselheiros tutelares que prestam   |\n");
+	printf("  |        | atendimento ao público                       |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    2   | Trabalhadores da educação do ensino básico em|\n");
+	printf("  |        | exercício nos ambientes escolares            |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    3   | Coveiros, atendentes e agentes funerários    |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    4   | Profissionais que trabalham em farmácias     |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    5   | Oficiais de justiça                          |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    6   | Taxistas e mototaxistas                      |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("  |    7   | Profissionais de limpeza pública             |\n");
+	printf("  +--------+----------------------------------------------+\n");
+	printf("\n");
+
+}
+
+void ImprimirComorbidades()
+{
+	printf("\n");
+	printf("                   +-----------------------------+               \n");
+	printf("                   |        Comorbidades         |               \n");
+	printf("  +----------------+--------------+--------------+--------------+\n");
+	printf("  |   Cardiopatia  |   Diabetes   |  Pneumopatia | Doença renal |\n"); 
+	printf("  +----------------+--------------+--------------+--------------+\n");
+	printf("  | Imunodepressão |   Obesidade  |     Asma     |   Puérperas  |\n");
+	printf("  +----------------+--------------+--------------+--------------+\n");
+	printf("\n");
+}
+
+void ExibePessoa (Pessoa *P) 
+{
+		printf("\n");
+		printf("  +----------------------+-------------------------------+\n");
+		printf("  |        Pessoa        |            Informação         |\n");
+		printf("  +----------------------+-------------------------------+\n");
+		printf("  |         Nome         | %-30s|\n",P->Nome);
+		printf("  |        Idade         | %-2d anos                       |\n",P->Idade);
+		printf("  |   Profissão (Grupo)  | Grupo %-1d                       |\n",P->Profissao);
+		printf("  | Comorbidades (1 | 0) | %-1d                             |\n",P->Comorbidades);
+		printf("  |         Fase         | %-1d                             |\n",P->Fase);
+		printf("  +----------------------+-------------------------------+\n");
+		printf("\n");
+}
+
+Celula *NovaCelula () 
+{
+  Celula *Aux = (Celula*)malloc(sizeof(Celula));
+  Aux->Proximo = NULL;  
+  return Aux;
+}
+
+void NovaFila (Fila *F) 
+{
+  Celula *Nova = NovaCelula();
+  F->Inicio = Nova;
+  F->Fim = Nova;
+  F->Tamanho = 0;
+}
+
+void CadastrarPessoa (Fila *F, Pessoa *P) 
+{
+  Celula *Nova = NovaCelula();  
+  Nova->Dado = *P;  
+  F->Fim->Proximo = Nova;
+  F->Fim = Nova;
+  F->Tamanho++;  
+}
+
+void ImprimePessoa(Pessoa P)
+{
+	printf("  | %d    | %-2d    | %d                 | %d                  | %-30s|\n",P.Fase,P.Idade,P.Profissao,P.Comorbidades,P.Nome);
+}
+
+void ImprimirLista(Fila *F)
+{
+	printf("  +------+-------+-------------------+--------------------+-------------------------------+\n");
+	printf("  | Fase | Idade | Profissão (Grupo) | Comorbidades (1|0) |              Nome             |\n");
+	printf("  +------+-------+-------------------+--------------------+-------------------------------+\n");
+	
+	Celula *Tmp = F->Inicio->Proximo;
+	while(Tmp != NULL)
+	{
+		ImprimePessoa(Tmp->Dado);
+		Tmp = Tmp->Proximo;
+	}
+	printf("  +------+-------+-------------------+--------------------+-------------------------------+\n");
+}
+
+void DefinirFase(Fila *FilaGeral, Pessoa *P)
+{
+	if(P->Idade > 75 || P->Profissao == 1)
+	{
+		P->Fase = 1;
+		ExibePessoa(P);
+	}
+	else if(P->Idade >= 60 && P->Idade <= 74)
+	{
+		P->Fase = 2;
+		ExibePessoa(P);
+	}
+	else if(P->Comorbidades == 1)
+	{
+		P->Fase = 3;
+		ExibePessoa(P);
+	}
+	else if(P->Idade < 18 || P->Gestante == 1 || P->Alergico == 1)
+	{
+		ForaFila();
+	}
+	else
+	{
+		P->Fase = 4;
+		ExibePessoa(P);
+	}
+}
+
+void ExibirMenu()
+{
+	printf("\n");
+	printf("  +------------------------------------------+\n");
+	printf("  |     Fila - Vacinação contra COVID-19     |\n");
+	printf("  +------------------------------------------+\n");
+	printf("  |    0 → Informações sobre o programa.     |\n");
+	printf("  |    1 → Cadastrar uma pessoa na fila.     |\n");
+	printf("  |    2 → Exibir a fila geral.              |\n");
+	printf("  |    3 → Exibir uma fila específica.       |\n");
+	printf("  |    4 → Links úteis.                      |\n");
+	printf("  |    5 → Sair.                             |\n");
+	printf("  +------------------------------------------+\n");
+	printf("\n");
+}
+
+void Info()
+{
+	printf("\n");
+	printf("  +-----------------------------------+\n");
+	printf("  |   Informações sobre o programa    |\n");
+	printf("  +-----------------------------------+---------------------------------+\n");
+	printf("  | O programa 'Vacinação contra COVID-19' tem o objetivo de aprimorar  |\n");
+	printf("  | nossas habilidades em torno do uso de ponteiros e arranjos, na dis- |\n");
+	printf("  | ciplina de AEDII.                                                   |\n");
+	printf("  +-----------------------------------+---------------------------------+\n");
+	printf("  |            Autores                |              Github             |\n");
+	printf("  +-----------------------------------+---------------------------------+\n");
+	printf("  |   Lincoln Corrêa Figueiredo Cruz  |           LincolnCFCruz         |\n");
+	printf("  |   Pedro Igor Martins dos Reis     |              ORZAYAZ            |\n");
+	printf("  |   Rafaela Mesquita Gontijo        |           rafamesquita          |\n");
+	printf("  |   Rafael Brandão Nunes            |          RafaelBrandao13        |\n");
+	printf("  +---------------------------------------------------------------------+\n");
+	printf("\n");
+}
+		
+void ForaFila()
+{
+	printf("  +--------------+\n");
+	printf("  | Fora da fila |\n");
+	printf("  +--------------+---------------+\n");
+	printf("  | A vacinação é contraindicada |\n");
+	printf("  | para menores de 18 anos,     |\n");
+	printf("  | gestantes, se ou teve alguma |\n");
+	printf("  | reção alérgica a uma dose    |\n");
+	printf("  | anterior a vacina ou a algum |\n");
+	printf("  | compontente da mesma.        |\n");
+	printf("  +------------------------------+\n");
+}
+
+void Links()
+{
+	printf(" +-------+\n");
+	printf(" | Links |\n");
+	printf(" +-------+---------+----------------------------------+\n");
+	printf(" |     Doenças     | https://saude.abril.com.br/      |\n");
+	printf(" | Grupos de risco | https://gauchazh.clicrbs.com.br/ |\n");
+	printf(" +-------+---------+----------------------------------+\n");
+}
+
+void Erro()
+{
+	printf("\n");
+	printf("  +----------------------------------+\n");
+	printf("  | Opção inválida, tente novamente. |\n");  
+	printf("  +----------------------------------+\n");
+	printf("\n");
+}
+
+void Reiniciar()
+{
+	printf("  +-------------------------+\n");
+	printf("  |  Deseja voltar ao menu  |\n");  
+	printf("  +-------------------------+\n");
+	printf("  |    1 - Sim | 0 - Não    |\n");
+	printf("  +-------------------------+\n");
+	printf("\n");
+}
+
+void Fim()
+{
+	printf("\n");
+	printf("  +-----------------+\n");
+	printf("  | Fim do programa |\n");
+	printf("  +-----------------+\n");
+	printf("\n");
+}
+
+
---
+++ Fonte/Ponteiro/Vacina.h
@@ -0,0 +1,53 @@
+#ifndef VACINA_H
+#define VACINA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Estrutura para armazenar os dados dos candidatos a vacinação.
+typedef struct Pessoa 
+{
+  char Nome[30];
+  int Idade;
+  int Comorbidades;
+  int Profissao;
+  int Fase;
+  int Gestante;
+  int Alergico;
+  int Exclusao;
+} Pessoa;
+
+//Estrutura que armazena as informações dos candidatos e o ponteiro que garante a sequência da fila.
+typedef struct Celula 
+{
+  Pessoa Dado;
+  struct Celula *Proximo;
+} Celula;
+
+//Estrutura que indica o tamanho, início e o final da fila.
+typedef struct Fila 
+{
+  Celula *Inicio;
+  Celula *Fim;
+  int Tamanho;
+} Fila;
+
+void ImprimirGrupos();
+void ImprimirComorbidades();
+void ExibePessoa (Pessoa *P);
+Celula *NovaCelula ();
+void NovaFila (Fila *F);
+void CadastrarPessoa (Fila *F, Pessoa *dado);
+void ImprimePessoa(Pessoa P);
+void ImprimirLista(Fila *F);
+void DefinirFase(Fila *F,Pessoa *P);
+void ForaFila();
+void ExibirMenu();
+void Erro();
+void Info();
+void Links();
+void Reiniciar();
+void Fim();
+
+#endif
